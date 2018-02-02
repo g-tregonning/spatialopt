6fb2ca80a2b7efd42f1d181c664f4e91f700a6e0
g-tregonning <g.tregonning2@ncl.ac.uk>
2018-02-02 15:46:41 +0000

    debugging

--- Constaints_v1.py
+++ Constaints_v1.py
@@ -10,7 +10,6 @@ Aspiration would be to calculate on average how many child solutions are removed
 
 def PTAL_Constraint(PTAL, Site, Density):
     # Catch the 150, 250 and 400 uha densities between PTAL 6B (2) and PTAL 4 (5)
-    print(PTAL)
     Site = tuple(Site)
     if Density >= 150 and PTAL[Site] < 6:
         return True
